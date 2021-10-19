23fe4f352dc32b53e63e356ae2aecf081b645ba4
AleksDu <aleksandr.dyshkin@gmail.com>
2021-10-19 09:37:26 +0300

    Update PokemonInfo.js

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -9,30 +9,37 @@ export default class PokemonInfo extends Component {
     state = {
         pokemon: null,
         loading: false,
+        error: null
     }
 
     componentDidUpdate(prevProps, prevState) {
         const prevName = prevProps.pokemonName;
         const nextName = this.props.pokemonName;
-        this.setState({loading: true})
+        
         if (prevName !== nextName) {
-            // console.log('изменилось имя покемона')
+            this.setState({ loading: true });
+            console.log('изменилось имя покемона')
             fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
                 .then(res => res.json())
                 .then(pokemon => this.setState({ pokemon }))
+                .catch(error => this.setState({error}))
             .finally( () => this.setState({loading: false}))
         }
     }
     
     render() {
-        const { pokemon, loading } = this.state;
+        const { pokemon, loading, error } = this.state;
         const {pokemonName} = this.props
 
         return <div>
             <h1>PokemonInfo</h1>
+            {error && <h1>халепа, покемона з таким імям { pokemonName} немає</h1>}
             {loading && <div>Загружаем</div>}
             {!pokemonName && <div>Введите имя покемона</div>}
-            {pokemon && <div>{ pokemon.name}</div>}
+            {pokemon && (<div>
+                <p>{pokemon.name}</p>
+                <img src={pokemon.sprites.other['official-artwork'].front_default} width='300' alt={pokemon.name} />
+            </div>)}
             </div>
     }
 }
